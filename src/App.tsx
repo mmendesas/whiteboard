cfbcf981f3b74ba1ad20c1d03057ec8116b9169c
Marcio Mendes <17890831+mmendesas@users.noreply.github.com>
2023-10-14 21:45:06 -0300

    feat: add integration with roughjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useLayoutEffect } from 'react';
+import rough from 'roughjs';
+
 import { useWindowResize } from './hooks/useWindowResize';
+
+const roughGenerator = rough.generator();
 
 function App() {
   const { width: canvasWidth, height: canvasHeight } = useWindowResize();
@@ -7,6 +11,12 @@ function App() {
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    // draw
+    const roughCanvas = rough.canvas(canvas);
+    const rectangle = roughGenerator.rectangle(100, 100, 100, 100);
+
+    roughCanvas.draw(rectangle);
 
     context.fillStyle = 'red';
     context.fillRect(50, 50, 100, 100);
